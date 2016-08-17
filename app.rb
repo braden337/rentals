e1a5714a03e97829c9c2f02887c54f8dd77614b8
Braden Jonsson <braden337@gmail.com>
2016-08-17 02:32:31 -0500

    made tons of progress

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'dm-sqlite-adapter'
 require 'bcrypt'
 require 'pry'
 
+require 'money'
+I18n.enforce_available_locales = false
+
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/rental.db")
 
 class User
@@ -23,22 +26,42 @@ class User
     self.password_hash = @password
   end
 
+  has n, :rentals
+
 end
 
-DataMapper.finalize
-User.auto_upgrade!
+class Rental
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :user_id, Integer, :required => true
+  property :address, String, :required => true
+  property :last_payment, DateTime
+
+  belongs_to :user
+  has n, :payments
+
+end
+
+class Payment
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :amount, Integer, :required => true
+  property :paid_at, DateTime, :required => true
+  property :rental_id, Integer, :required => true
+
+  belongs_to :rental
+
+end
+
+DataMapper.finalize.auto_upgrade!
+# User.auto_upgrade!
 
 
 
 class RentalApp < Sinatra::Base
 
-  # use Rack::Session::Cookie, :key => 'rack.session',
-  #                            :domain => 'localhost',
-  #                            :path => '/',
-  #                            :expire_after => 2592000, # In seconds
-  #                            :secret => 'wgckibzwewshlmkeniyazktmlnyskgirpxkaotmfhchczjicfa'
-  # set :bind, '0.0.0.0'
-  # set :port, 3000
   enable :sessions
   set :session_secret, 'wgckibzwewshlmkeniyazktmlnyskgirpxkaotmfhchczjicfa'
 
@@ -46,13 +69,47 @@ class RentalApp < Sinatra::Base
     def logged_in?
       !!session[:id]
     end
+    def money(amount)
+      Money.new(amount, 'CAD').format
+    end
   end
 
   get '/' do
-    # binding.pry
     redirect '/login' if !logged_in?
     @user = User.get(session[:id])
+    @rentals = @user.rentals.all(:order => [ :last_payment ])
     erb :index
+    # binding.pry
+  end
+
+  post '/' do
+    # m = Money.new(params[:amount].to_f*100, 'CAD')
+    # binding.pry
+    p = Payment.new(:rental_id => params[:rental], :amount => params[:amount].to_f*100, :paid_at => Time.now)
+    p.save
+    r = Rental.get(p.rental_id)
+    r.update(:last_payment => p.paid_at)
+    r.save
+    redirect "/rental/#{p.rental_id}"
+  end
+
+  post '/rental' do
+    Rental.create(:address => params[:address], :user_id => session[:id])
+    redirect '/'
+  end
+
+  get '/rental/new' do
+    erb :'rentals/new'
+  end
+
+  get '/rental/:id' do
+    @rental = Rental.get(params[:id])
+    redirect '/' unless logged_in? && session[:id] == @rental.user_id
+    @user = User.get(session[:id])
+    @payments = @rental.payments
+    @sum =  @payments.map{|x| x.amount}.reduce(:+)
+    # binding.pry
+    erb :'rentals/show'
   end
 
   get '/login' do
@@ -76,12 +133,3 @@ class RentalApp < Sinatra::Base
   end
 
 end
-
-
-
-
-
-
-
-
-
